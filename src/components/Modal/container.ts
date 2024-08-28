d9b855bdbb1216d8bdab800e35bb9d58bde3764c
pki071120 <ddametyy@gmail.com>
2024-08-28 14:26:43 +0900

    add modalContainer

---
+++ src/components/Modal/container.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(51, 51, 51, 0.4);
+`;
+
+export default Container;
