d46901b1b5fdad743e0e3df3a17e13d73a83dd20
pki071120 <ddametyy@gmail.com>
2024-09-22 22:33:26 +0900

    add colorTheme

---
+++ src/style/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+  color: {
+    main: '#36C05C',
+    black: '#333333',
+    white: '#ffffff',
+    gray: {
+      900: '#4F4F51',
+      800: '#666669',
+      700: '#828387',
+      600: '#8f9094',
+      500: '#A5A6A9',
+      400: '#B4B5B7',
+      300: '#DBDCDE',
+      200: '#EFF0F2',
+      100: '#F5F6F8',
+    },
+    system: '#E53A3A',
+  },
+};
+
+export default theme;
